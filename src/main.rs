5b02748760d8f4c5c7857e6ec09d1c3be8ba29e7
Justin Duplessis <drfoliberg@gmail.com>
2016-05-15 17:53:29 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,13 @@ fn read(pin_num: u64) -> sysfs_gpio::Result<()> {
         try!(pin.set_direction(Direction::Low));
         pin.set_value(0).unwrap();
         let t1 = time::now();
-        sleep(Duration::new(0, 30 * 1000000)); // Sleep 30ms
+        sleep(Duration::new(0, 20 * 1000000)); // Sleep 30ms
 
-        let t2 = time::now();
         pin.set_value(1).unwrap();
+        let t2 = time::now();
         sleep(Duration::new(0, 30 * 1000)); // Sleep 30us
-
         let t3 = time::now();
+
         try!(pin.set_direction(Direction::In));
         let t4 = time::now();
         for _ in 1..101 {
@@ -32,6 +32,7 @@ fn read(pin_num: u64) -> sysfs_gpio::Result<()> {
             let val = try!(pin.get_value());
             let t6 = time::now();
             println!("{:?} took {}", val, t6 - t5);
+            sleep(Duration::new(0, 10 * 1000)); // Sleep 10us
         }
         println!("{} {} {}", t2 - t1, t3 - t2, t4 - t3);
         Ok(())
