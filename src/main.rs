921a655d680ee39035dde88462f9395f82bd944b
Justin Duplessis <drfoliberg@gmail.com>
2016-05-15 17:38:19 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,6 @@ fn read(pin_num: u64) -> sysfs_gpio::Result<()> {
         for _ in 1..101 {
             let val = try!(pin.get_value());
             println!("{:?}", val);
-            sleep(Duration::new(0, 50));
         }
         println!("{} {} {}", t2 - t1, t3 - t2, t4 - t3);
         Ok(())
