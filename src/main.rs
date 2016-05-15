f704361bb800a65157f80b8eb3b758a5854e1e21
Justin Duplessis <drfoliberg@gmail.com>
2016-05-15 17:21:47 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -12,30 +12,6 @@ pub fn main() {
 }
 
 fn read(pin_num: u64) -> sysfs_gpio::Result<()> {
-    // let pin = Pin::new(pin_num);
-    // pin.with_exported(|| {
-    //     pin.set_value(0).unwrap();
-    //     sleep(Duration::from_millis(30)); // Sleep 30ms
-    //     pin.set_value(1).unwrap();
-    //     sleep(Duration::new(0, 30)); // Sleep 30us
-    // })
-    //
-    // pin.with_exported(|| {
-    //     try!(pin.set_direction(Direction::In));
-    //
-    //     let mut i = 0;
-    //     while i < 100 {
-    //         let val = try!(pin.get_value());
-    //         println!("Pin State: {}",
-    //         if val == 0 {
-    //             "Low"
-    //         } else {
-    //             "High"
-    //         });
-    //         i += 1;
-    //     }
-    //     Ok(())
-    // }).unwrap();
     let pin = Pin::new(pin_num);
     pin.with_exported(|| {
         try!(pin.set_direction(Direction::Low));
@@ -48,20 +24,12 @@ fn read(pin_num: u64) -> sysfs_gpio::Result<()> {
 
     pin.with_exported(|| {
         try!(pin.set_direction(Direction::In));
-        let mut prev_val: u8 = 255;
-        loop {
+        for i in 1..101 {
             let val = try!(pin.get_value());
-            if val != prev_val {
-                println!("Pin State: {}",
-                         if val == 0 {
-                             "Low"
-                         } else {
-                             "High"
-                         });
-                prev_val = val;
-            }
-            // sleep(Duration::from_millis(10));
+            println!("{:?}", val);
+            sleep(Duration::new(0, 50)); // Sleep 30us
         }
+        Ok(())
     }).unwrap();
     Ok(())
 }
