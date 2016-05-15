b67a4b166553e821b8221bbaf5caf5216ce29285
Justin Duplessis <drfoliberg@gmail.com>
2016-05-15 18:02:03 -0400

    wip

--- src/main.rs
+++ src/main.rs
@@ -15,27 +15,24 @@ pub fn main() {
 fn read(pin_num: u64) -> sysfs_gpio::Result<()> {
     let pin = Pin::new(pin_num);
     pin.with_exported(|| {
+        let mut bits = vec![0];
         try!(pin.set_direction(Direction::Low));
         pin.set_value(0).unwrap();
-        let t1 = time::now();
         sleep(Duration::new(0, 20 * 1000000)); // Sleep 30ms
 
         pin.set_value(1).unwrap();
-        let t2 = time::now();
         sleep(Duration::new(0, 1));
-        let t3 = time::now();
 
         try!(pin.set_direction(Direction::In));
-        let t4 = time::now();
         let mut last = 1;
         for _ in 1..512 {
             let val = try!(pin.get_value());
             if val != last {
                 last = val;
-                println!("{}", val);
+                bits.push(val);
             }
         }
-        println!("{} {} {}", t2 - t1, t3 - t2, t4 - t3);
+        println!("{:?}", bits);
         Ok(())
     }).unwrap();
     Ok(())
